64e03fa494c289999a6d90039019df52aaee8e6d
Christopher Amadra Titus <kris.skyfallgraphix@gmail.com>
2023-08-02 12:12:30 +0300

    Added section for patron, concepts, opportunities, partner associations and edited map

--- website/kyucsa/static/js/main.js
+++ website/kyucsa/static/js/main.js
@@ -226,3 +226,33 @@
   });
 
 })()
+
+
+//Owl Carousal for Partners Logo Slider
+$('#owl-carousel').owlCarousel({
+loop: true,
+margin: 30,
+dots: false,
+nav: false,
+autoplay:true,
+autoplayTimeout:5000,
+autoplayHoverPause:true,
+items : 5,
+responsive: {
+0: {
+  items: 2
+},
+
+600: {
+  items: 3
+},
+
+1024: {
+  items: 5
+},
+
+1366: {
+  items: 5
+}
+}
+})
