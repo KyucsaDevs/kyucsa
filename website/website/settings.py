614420b326f85e2def3aab906ebd580a77d0d0a6
Christopher Amadra Titus <kris.skyfallgraphix@gmail.com>
2023-11-11 01:47:11 +0300

    Installed WhiteN

--- website/website/settings.py
+++ website/website/settings.py
@@ -111,6 +111,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
