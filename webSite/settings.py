3cde22fb6d8972dfaf9e2d329d64c7d3d784e2ba
Christopher Amadra Titus <kris.skyfallgraphix@gmail.com>
2024-06-07 22:06:21 +0300

    Configured url

--- webSite/settings.py
+++ webSite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "django-insecure-k126hu3z=mk2s!f_-*#$w4e^7zs%xhu02uhhd1)@9xoz#3vgl2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', )
